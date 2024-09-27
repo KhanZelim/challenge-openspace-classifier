87605148c48fd25d015a617d80e2a96462438a71
zelim <zelimkhanjachichanov@hotmail.com>
2024-09-27 09:21:07 +0200

    Updates docstrings

--- utils/table.py
+++ utils/table.py
@@ -2,7 +2,7 @@ class Seat:
     """Class representing one seat"""
 
     def __init__(self):
-        """Consructor of our class"""
+        """Constructor of our class"""
         self.free = True
         self.occupant = ""
 
@@ -10,18 +10,18 @@ class Seat:
         """
         Function that will set the occupant to the given name (in param)
         
-        :param name: A string
+        :param name: A string representing the occupant's name
         """
         self.occupant = name
         self.free = False
 
     def remove_occupant(self):
-        """Function that will set the occupant to a blank string"""
+        """Function that will set the occupant to a blank string and sets the seat free"""
         self.occupant = ""
         self.free = True
 
     def __str__(self):
-        """"""
+        """A string representation of the seat's status"""
         if self.free:
             return f"is unoccupied"
         else:
@@ -37,7 +37,7 @@ class Table:
         :param capacity: An int representing the capacity
         """
         self.capacity = capacity
-        self.seats = [Seat() for i in range(capacity)]
+        self.seats = [Seat() for _ in range(capacity)]
 
     def has_free_spot(self):
         """Function that will check if there are any free spots"""
@@ -55,9 +55,9 @@ class Table:
                 break
 
     def left_capacity(self):
-        """Function that checks the capacity left"""
+        """Function that returns the capacity left"""
         return self.capacity
 
     def __str__(self):
-        """"""
+        """A string representation of the table's status"""
         return f"Capacity: {self.capacity}"

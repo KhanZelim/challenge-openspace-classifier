17f700461f4d5c41cc7b7b876b44281ee5ea3b80
Ihor1654 <122479591+Ihor1654@users.noreply.github.com>
2024-09-27 16:10:29 +0200

    Finall version 1

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from utils.openspace import Openspace
 import pandas as pd
+from os import path as p
+#"/Users/igorafanasIev/Desktop/becode_projects/challenge-openspace-classifier/challenge-openspace-classifier/new_colleagues.csv"
 
 classroom = Openspace()
 colleagueslist = []
-
-csvColleagues = pd.read_csv("/Users/igorafanasIev/Desktop/becode_projects/challenge-openspace-classifier/challenge-openspace-classifier/new_colleagues.csv", header=None)
-for row in csvColleagues.itertuples():
-    colleagueslist.append(row[1])
-print(colleagueslist)
+while True:
+    path = input('Enter file path')
+    if p.exists(path):
+        csvColleagues = pd.read_csv(path, header=None)
+        for row in csvColleagues.itertuples():
+            colleagueslist.append(row[1])
+        break
+    else:
+        print('Incorrect path,try again')
 
 classroom.organize(colleagueslist)
 classroom.dysplay()
+filename = input('Enter the name of file you gonna save')
+try:
+    classroom.store('filename')
+except:
+    print('Something wrong')
